bf2e8244cbc1ef2c1c162713f5c710bb20dc9433
jesusfb <jfigueroa.beltran@gmail.com>
2023-08-12 23:42:02 -0700

    Update dbConnect.js

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnect = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_URI);
+    const conn = await mongoose.connect("mongodb+srv://jesusfb:Dove3229-@cluster0.yx9sjqo.mongodb.net/mern");
     console.log(`MongoDB Connected to ${conn.connection.host} ,biatch!`);
   } catch (error) {
     console.log(`${error.message}`);
